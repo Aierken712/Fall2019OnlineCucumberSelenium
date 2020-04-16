4e655366ded4bcce6f599d5567587c95293f3d82
Aierken712 <ershad0419@gmail.com>
2020-04-15 22:04:20 -0400

    Wednesday Cucumber class

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -6,10 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com/vytrack/step_definitions",
-        features = "src/test/resources/features",
-        dryRun = true,
-        strict = true
+        glue = "com\\vytrack\\step_definitions",
+        features = "src\\test\\resources\\features",
+        dryRun = false,
+        strict = false,
+        // only run which scenario has this tags
+        tags = "", // tags = "not @driver" --> run everything
+        plugin = "html:target/cucumber-html-report"
+
 )
 public class CucumberRunner {
 
