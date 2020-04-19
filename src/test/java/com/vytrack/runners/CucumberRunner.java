7ff02796fa68f5e5b0bb7cb2a68b866638b92d3c
Aierken712 <ershad0419@gmail.com>
2020-04-19 16:28:20 -0400

    Sunday Class

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         tags = "@create_calendar_event_2", // tags = "not @driver" --> run everything
         plugin = {
                 "html:target/default-report",
-                "json:target/cucumber.json"
+                "json:target/cucumber1.json"
         }
 
 )
